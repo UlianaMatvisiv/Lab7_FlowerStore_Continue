227cff2150e774d58c5d644c7dd99507e0c07e41
UlianaMatvisiv <116552600+UlianaMatvisiv@users.noreply.github.com>
2023-10-26 22:01:11 +0300

    fix 5

--- src/main/java/ucu/edu/ua/flowerstore/delivery/DeliveryController.java
+++ src/main/java/ucu/edu/ua/flowerstore/delivery/DeliveryController.java
@@ -12,7 +12,7 @@ public class DeliveryController {
     
     @GetMapping("/delivery")
     public String getDeliveryStrategies() {
-        return "Ways of delivery " +
-            List.of(DHLDeliveryStrategy.class, PostDeliveryStrategy.class);
+        return "Ways of delivery " 
+            + List.of(DHLDeliveryStrategy.class, PostDeliveryStrategy.class);
     }
 }
