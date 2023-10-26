0ab6e5f2b406d185eadb0c24e90866adf529c67f
UlianaMatvisiv <116552600+UlianaMatvisiv@users.noreply.github.com>
2023-10-26 22:01:48 +0300

    fix 5

--- src/test/java/ucu/edu/ua/flowerstore/PaymentTest.java
+++ src/test/java/ucu/edu/ua/flowerstore/PaymentTest.java
@@ -5,10 +5,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ucu.edu.ua.flowerstore.payment.PayPalPaymentStrategy;
 import ucu.edu.ua.flowerstore.payment.CreditCardPaymentStrategy;
-
 public class PaymentTest {
     private CreditCardPaymentStrategy creditCard;
     private PayPalPaymentStrategy payPal;
+    private static final double FIRST_PRICE = 578.7;
+    private static final double SECOND_PRICE = 221.33;
 
     @BeforeEach
     public void init() {
@@ -16,13 +17,8 @@ public class PaymentTest {
         payPal = new PayPalPaymentStrategy();
     }
 
-    // Define constants for the test prices
-    private static final double FIRST_PRICE = 578.7;
-    private static final double SECOND_PRICE = 221.33;
-
     @Test
     public void testPaymentStrategies() {
-        // Use the constants in your test cases
         Assertions.assertEquals(FIRST_PRICE, creditCard.pay(FIRST_PRICE));
         Assertions.assertEquals(SECOND_PRICE, payPal.pay(SECOND_PRICE));
     }
