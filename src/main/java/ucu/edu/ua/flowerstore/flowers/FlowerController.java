aaeea885837d9b5a03431510af648b690fa3b845
UlianaMatvisiv <116552600+UlianaMatvisiv@users.noreply.github.com>
2023-10-26 21:35:15 +0300

    fix 3

--- src/main/java/ucu/edu/ua/flowerstore/flowers/FlowerController.java
+++ src/main/java/ucu/edu/ua/flowerstore/flowers/FlowerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/flower")
 public class FlowerController {
+
 	@GetMapping("/list")
 	public List<Flower> getFlowers() {
 		return List.of(new Flower(0.8, 
