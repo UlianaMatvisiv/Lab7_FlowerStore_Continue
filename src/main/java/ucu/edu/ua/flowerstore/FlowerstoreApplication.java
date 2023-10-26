8ae9fd9b54336758dddfb2d6bf94d0610a8bbd14
UlianaMatvisiv <116552600+UlianaMatvisiv@users.noreply.github.com>
2023-10-26 21:34:43 +0300

    fix 3

--- src/main/java/ucu/edu/ua/flowerstore/FlowerstoreApplication.java
+++ src/main/java/ucu/edu/ua/flowerstore/FlowerstoreApplication.java
@@ -2,10 +2,12 @@ package ucu.edu.ua.flowerstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FlowerstoreApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(FlowerstoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FlowerstoreApplication.class, args);
+    }
 }
