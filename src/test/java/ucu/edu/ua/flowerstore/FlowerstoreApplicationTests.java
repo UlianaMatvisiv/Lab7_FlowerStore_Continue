28b884fd9fff7e5acdab0694c2e4f0d9bdd4f152
UlianaMatvisiv <116552600+UlianaMatvisiv@users.noreply.github.com>
2023-10-26 20:35:32 +0300

    applicationtest

--- src/test/java/ucu/edu/ua/flowerstore/FlowerstoreApplicationTests.java
+++ src/test/java/ucu/edu/ua/flowerstore/FlowerstoreApplicationTests.java
@@ -1,1 +1,13 @@
+package ucu.edu.ua.flowerstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
 }
