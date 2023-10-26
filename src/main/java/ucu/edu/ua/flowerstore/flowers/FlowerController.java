8a8c90b57f1af2b873a808d80f698d44198f7645
UlianaMatvisiv <116552600+UlianaMatvisiv@users.noreply.github.com>
2023-10-26 20:31:46 +0300

    flowercontroller

---
+++ src/main/java/ucu/edu/ua/flowerstore/flowers/FlowerController.java
@@ -0,0 +1,18 @@
+package ucu.edu.ua.flowerstore.flowers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/flower")
+public class FlowerController {
+	@GetMapping("/list")
+	public List<Flower> getFlowers(){
+		return List.of(new Flower(0.8, FlowerColor.RED, 45, FlowerType.ROSE));
+	}
+
+}
+
